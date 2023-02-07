321f9fc2e2a4a6858091c873d82731fa8618406f
y nhi <nguyenthiynhi010601@gmail.com>
2023-02-07 22:45:50 +0700

    use-effect

--- Section8-react-hooks/src/App.js
+++ Section8-react-hooks/src/App.js
@@ -1,10 +1,14 @@
 // import ColorBox from 'components/ColorBox';
 
-import React from 'react';
+
+import React, { useEffect } from 'react';
 import { useState } from 'react';
+import queryString from 'query-string';
 import './App.css';
+import PostList from './components/PostList';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import Pagination from './components/Pagination';
 
 function App() {
   const [todoList, setTodoList]= useState([
@@ -12,6 +16,42 @@ function App() {
     {id:2, title: 'We love Easy Frontend'},
     {id:3, title: 'They love Easy Frontend'}
   ]);
+
+  const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page:1,
+    _limit: 10,
+    _totalRows:11
+  });
+  const [filters, setFilter] = useState({
+    _limit: 10,
+    _page :1,
+  })
+  // query-string -> _limt=10&page=1 
+
+  useEffect(()=> {
+    async function fetchPostList() {
+      try {
+        //api
+        const paramsString = queryString.stringify(filters);
+        const requestURL = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;// truyền biến
+        const response = await fetch(requestURL);
+        const responseJSON = await response.json();
+        console.log({responseJSON})
+  
+        const {data, pagination} = responseJSON;
+        setPostList(data);
+        setPagination(pagination);
+      } catch (error) {
+        console.log(error.message);
+      }
+     
+    }
+
+    fetchPostList();
+
+  },[filters])
+  
   function handleTodoClick(todo) {
     const index = todoList.findIndex(x =>x.id === todo.id);
     if(index <0) return;
@@ -29,12 +69,24 @@ function App() {
     newTodoList.push(newTodo);
     setTodoList(newTodoList);
   }
+  function handlePageChange(newPage) {
+    console.log('new page:',newPage);
+    setFilter({
+      ...filters,
+      _page: newPage
+    })
+  }
   return (
     <div className="App">
       <div>React Hook</div>
       {/* <ColorBox /> */}
-      <TodoForm onSubmit={handleTodoFormSubmit}/>
-      <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
+      {/* <TodoForm onSubmit={handleTodoFormSubmit}/>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick}/> */}
+      <PostList posts={postList}/>
+      <Pagination 
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
